8009d5e2742f49d1741f6367cd3e284761ac6cec
Glenn <54360473+Kurohayabusa-r@users.noreply.github.com>
2021-05-24 00:36:00 +0700

    News feature, not connected to db yet

---
+++ news/news.py
@@ -0,0 +1,63 @@
+import requests
+from bs4 import BeautifulSoup
+from google.cloud import storage
+from google.cloud import firestore
+import os
+import time
+
+limit = 5
+
+def read_news():
+    try:
+        r = requests.get('https://www.bmkg.go.id/tag/?tag=berita-utama&lang=ID')
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.content, 'html.parser')
+            posts = soup.findAll('div', class_='row margin-bottom-30')
+            for post in posts[:limit]:
+                link = str(post.find('a')['href']).replace('..', 'https://www.bmkg.go.id', 1)
+                if link not in blacklist:
+                    title = post.find('a').text
+                    published = post.find('li').text.strip()
+                    find_image = post.find('img', class_='img-responsive')['data-original']
+
+                    print(title)
+                    print(link)
+                    print(published)
+                    print(find_image)
+                    print()
+
+                    blacklist.append(link)
+
+                    with open('seen_posts.txt', 'a') as f:
+                        f.write(link + '\n')
+                        f.close()
+
+                    blob.upload_from_filename('seen_posts.txt')
+        
+        time.sleep(3)
+        return
+
+    except Exception as e:
+        print(e)
+
+
+def blacklisted_posts():
+    if not os.path.isfile('seen_posts.txt'):
+        blacklist = []
+    else:
+        with open('seen_posts.txt', 'r') as f:
+            blacklist = f.read()
+            blacklist = blacklist.split("\n")
+            f.close()
+
+    return blacklist
+
+
+client = storage.Client()
+bucket = client.get_bucket('b21cap0397')
+blob = bucket.blob('seen_posts.txt')
+blob.download_to_filename('seen_posts.txt')
+
+blacklist = blacklisted_posts()
+while True:
+    read_news()
