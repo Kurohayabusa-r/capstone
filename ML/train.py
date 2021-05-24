c483cb36a508eb2aaeffd799b87fad27c9c80b25
DeathWish99 <DeathWish99@users.noreply.github.com>
2021-05-24 09:52:15 +0700

    Import python libs

---
+++ ML/train.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
+from sklearn.preprocessing import MinMaxScaler
+
+print("Hello")
