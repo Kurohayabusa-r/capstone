bcf24b1ac770f6f218c279127802f05e7dce353f
DeathWish99 <DeathWish99@users.noreply.github.com>
2021-05-24 15:01:42 +0700

    process csvs to usable data

--- ML/train.py
+++ ML/train.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 from matplotlib.pylab import rcParams
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 from sklearn.preprocessing import MinMaxScaler
 
 print("Hello")
+
+dataset = pd.read_csv('./DataModel.csv')
+
+print(dataset)
