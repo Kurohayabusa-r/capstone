d86635a083118f4740db9738225e16826af2ef50
DeathWish99 <DeathWish99@users.noreply.github.com>
2021-05-24 15:35:20 +0700

    randomly select data into one dataset

--- ML/train.py
+++ ML/train.py
@@ -1,15 +1,35 @@
 import pandas as pd
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 import os
 from matplotlib.pylab import rcParams
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
-from sklearn.preprocessing import MinMaxScaler
+import sklearn.preprocessing as skpre
 
 print("Hello")
 
-dataset = pd.read_csv('./DataModel.csv')
+dataset2009 = pd.read_csv('./csv/2009.csv')
+dataset2010 = pd.read_csv('./csv/2010.csv')
+dataset2011 = pd.read_csv('./csv/2011.csv')
+dataset2012 = pd.read_csv('./csv/2012.csv')
+dataset2013 = pd.read_csv('./csv/2013.csv')
+dataset2014 = pd.read_csv('./csv/2014.csv')
+dataset2015 = pd.read_csv('./csv/2015.csv')
+dataset2016 = pd.read_csv('./csv/2016.csv')
+dataset2017 = pd.read_csv('./csv/2017.csv')
+dataset2018 = pd.read_csv('./csv/2018.csv')
 
-print(dataset)
+datasetTotal = pd.DataFrame({})
+datasetTotal = datasetTotal.append(dataset2009.sample(1000))
+datasetTotal = datasetTotal.append(dataset2010.sample(1000))
+datasetTotal = datasetTotal.append(dataset2011.sample(1000))
+datasetTotal = datasetTotal.append(dataset2012.sample(1000))
+datasetTotal = datasetTotal.append(dataset2013.sample(1000))
+datasetTotal = datasetTotal.append(dataset2014.sample(1000))
+datasetTotal = datasetTotal.append(dataset2015.sample(1000))
+datasetTotal = datasetTotal.append(dataset2016.sample(1000))
+datasetTotal = datasetTotal.append(dataset2017.sample(1000))
+datasetTotal = datasetTotal.append(dataset2018.sample(1000))
+print(datasetTotal)
