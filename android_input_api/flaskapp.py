ffe67c6c32cdf6d987794d74b132fe945a39ce77
Glenn <54360473+Kurohayabusa-r@users.noreply.github.com>
2021-05-28 20:44:05 +0700

    Fixed various API bugs

--- android_input_api/flaskapp.py
+++ android_input_api/flaskapp.py
@@ -12,18 +12,18 @@ def root():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        data = request.json['instances']
-        
-        dataset2009 = pd.read_csv('./csv/2009.csv')
-        dataset2010 = pd.read_csv('./csv/2010.csv')
-        dataset2011 = pd.read_csv('./csv/2011.csv')
-        dataset2012 = pd.read_csv('./csv/2012.csv')
-        dataset2013 = pd.read_csv('./csv/2013.csv')
-        dataset2014 = pd.read_csv('./csv/2014.csv')
-        dataset2015 = pd.read_csv('./csv/2015.csv')
-        dataset2016 = pd.read_csv('./csv/2016.csv')
-        dataset2017 = pd.read_csv('./csv/2017.csv')
-        dataset2018 = pd.read_csv('./csv/2018.csv')
+        data = request.json.get('instances')
+
+        dataset2009 = pd.read_csv('/home/glennyohanes17/flaskapp/csv/2009.csv')
+        dataset2010 = pd.read_csv('/home/glennyohanes17/flaskapp/csv/2010.csv')
+        dataset2011 = pd.read_csv('/home/glennyohanes17/flaskapp/csv/2011.csv')
+        dataset2012 = pd.read_csv('/home/glennyohanes17/flaskapp/csv/2012.csv')
+        dataset2013 = pd.read_csv('/home/glennyohanes17/flaskapp/csv/2013.csv')
+        dataset2014 = pd.read_csv('/home/glennyohanes17/flaskapp/csv/2014.csv')
+        dataset2015 = pd.read_csv('/home/glennyohanes17/flaskapp/csv/2015.csv')
+        dataset2016 = pd.read_csv('/home/glennyohanes17/flaskapp/csv/2016.csv')
+        dataset2017 = pd.read_csv('/home/glennyohanes17/flaskapp/csv/2017.csv')
+        dataset2018 = pd.read_csv('/home/glennyohanes17/flaskapp/csv/2018.csv')
 
         datasetTotal = pd.DataFrame({})
         datasetTotal = datasetTotal.append(dataset2009)
@@ -43,13 +43,12 @@ def predict():
         sc = StandardScaler()
         X = sc.fit(X)
         formatted = sc.transform(data)
-
         url = "https://predict-feizm4s2ta-et.a.run.app/v1/models/model/versions/1:predict"
-        input_data = {"instances": formatted}
+        input_data = {"instances": formatted.tolist()}
         x = requests.post(url, json=input_data)
         return x.text
     except Exception as e:
         return "Error: {}".format(e)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
