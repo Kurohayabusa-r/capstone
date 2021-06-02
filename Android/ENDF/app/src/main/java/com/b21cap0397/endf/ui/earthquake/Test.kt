4f3e41a4b24c44738d669da10b1f6fcfccc2bd1b
Febri <achmadfebryanto@gmail.com>
2021-06-02 22:50:18 +0700

    Change app icon

---
+++ Android/ENDF/app/src/main/java/com/b21cap0397/endf/ui/earthquake/Test.kt
@@ -0,0 +1,12 @@
+package com.b21cap0397.endf.ui.earthquake
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun main() {
+
+}
