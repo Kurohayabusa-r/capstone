cd9f1a8546f5c2111ce8308a25577961bb33f18f
Muhammad Irvan <mirvan2599@gmail.com>
2021-06-04 10:45:36 +0700

    Adding language settings

--- Android/ENDF/app/src/main/java/com/b21cap0397/endf/MainActivity.kt
+++ Android/ENDF/app/src/main/java/com/b21cap0397/endf/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.b21cap0397.endf
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -24,5 +28,17 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_language_settings) {
+            val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(mIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
